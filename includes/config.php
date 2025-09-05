c6ab292eb34c1ce35e5a56f4041a3a08c6284753
Warorua <waroruaalex640@gmail.com>
2025-09-05 22:22:00 +0300

    fgukfu

--- includes/config.php
+++ includes/config.php
@@ -5,4 +5,4 @@ define('DB_USER', 'u854855859_redHat');
 define('DB_PASS', 'ccu*4HhD4^Cm');
 
 // define('APP_URL', 'https://yourdomain.tld/payments-app'); // <- change to your real base URL
-define('APP_URL', 'http://localhost/kever_b'); // <- change to your real base URL
+define('APP_URL', 'https://bombardier.romulus.website'); // <- change to your real base URL
